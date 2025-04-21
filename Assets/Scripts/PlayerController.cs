3bbdddc9a2f2070f072fe1bf26e363d9ea321fbb
Elliott Kelly <ek37@iu.edu>
2025-04-21 17:29:17 -0400

    Updated player controller physics values
    The player controller now matches the behavior of the original game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
-    private const float GRAVITY = 20;
-    private const float MAXFALL = 1500;
-    private const float MAXSPEED = 300;
-    private const float JUMPFORCE = 590;
-    private const float ACCEL = 200;
+    private const float MAXFALL = 10.5f;
+    private const float MAXSPEED = 3.8f;
+    [SerializeField] private float JUMPFORCE = 2.5f;
+    private const float ACCEL = 2.5f;
+    private bool canJump = true;
 
-    private Vector2 movement = Vector2.zero;
+    private Vector3 movement = Vector3.zero;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
     {
         if (Input.GetAxis("Horizontal") > 0)
         {
@@ -34,28 +41,33 @@ public class PlayerController : MonoBehaviour
         else
         {
             //Play standing animation
-            movement.x = Mathf.Lerp(movement.x, 0, 0.2f);
+            movement.x = Mathf.Lerp(movement.x, 0, 0.8f);
         }
-        if (IsOnFloor() && Input.GetButtonDown("Jump"))
+        if (IsOnFloor() && Input.GetButton("Jump") && canJump)
         {
-            movement.y -= JUMPFORCE;
+            rb.AddForce(JUMPFORCE * Vector2.up, ForceMode2D.Impulse);
         }
-    }
 
-    private void FixedUpdate()
-    {
         movement.x = Mathf.Clamp(movement.x, -MAXSPEED, MAXSPEED);
-        if (movement.y < MAXFALL)
+        if (rb.linearVelocityY > MAXFALL)
         {
-            movement.y += GRAVITY;
+            rb.linearVelocityY = MAXFALL;
         }
+        transform.position += movement * Time.deltaTime;
     }
 
     private bool IsOnFloor()
     {
         RaycastHit2D hit;
-        hit = Physics2D.Raycast(transform.position, Vector2.down, 0.3f);
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
         return hit;
+    }
+
+    private IEnumerator CanJump()
+    {
+        canJump = false;
+        yield return new WaitForSeconds(0.5f);
+        canJump = true;
     }
 	
 }
